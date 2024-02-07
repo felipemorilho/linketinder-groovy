23390346ac0b097f5406147bea6780745d94bc1b
Felipe Morilho <felipemdecastro@gmail.com>
2024-02-07 13:36:04 -0300

    feat: add manual registration for company

--- src/com/acelerazg/controller/UserController.groovy
+++ src/com/acelerazg/controller/UserController.groovy
@@ -13,15 +13,14 @@ class UserController {
 
     void registerApplicant(ApplicantModel applicant) {
         applicants.add(applicant)
-        println("${applicant.getName()}, seu cadastro foi efetuado!")
     }
 
     void registerCompany(CompanyModel company) {
         companies.add(company)
-        println("${company.getName()}, seu cadastro foi efetuado!")
     }
 
-    ApplicantModel manualRegister() {
+    @SuppressWarnings('GroovyMissingReturnStatement')
+    ApplicantModel manualRegisterApplicant() {
         println("\u001b[34m Cadastro de Candidato \u001B[0m")
 
         print("Digite o nome: ")
@@ -68,7 +67,59 @@ class UserController {
             }
         }
 
+        println("\n\u001B[36m${name}, seu cadastro foi efetuado!\u001B[0m")
         def newApplicant = new ApplicantModel(name, email, state, cep, cpf, age, description, skills)
+    }
+
+    CompanyModel manualRegisterCompany() {
+        println("\u001b[34m Cadastro de Candidato \u001B[0m")
+
+        print("Digite o nome: ")
+        String name = scan.nextLine()
+
+        print("Digite o email: ")
+        String email = scan.nextLine()
+
+        print("Digite seu CNPJ (Ex: 00.000.000/0000-00): ")
+        String cnpj = scan.nextLine()
+
+        print("Digite o estado em que reside: ")
+        String state = scan.nextLine()
+
+        print("Digite o país em que reside: ")
+        String country = scan.nextLine()
+
+        print("Digite seu CEP (Ex: 00000-000): ")
+        String cep = scan.nextLine()
+
+        print("Aqui, faça uma descrição sobre a empresa: ")
+        String description = scan.nextLine()
+
+        def skills = []
+        while (true) {
+            println('''Escolha suas competências: 
+
+            PYTHON, JAVA, SPRING_FRAMEWORK, 
+            ANGULAR, GROOVY, JAVASCRIPT ou TYPESCRIPT
+
+            Digite "sair" para finalizar.''')
+
+            print("Digite a que deseja adicionar: ")
+            String readSkill = scan.nextLine()
+
+            if (readSkill.equalsIgnoreCase('sair')) {
+                break
+            }
+            try {
+                def skillName = Skill.valueOf(readSkill.toUpperCase())
+                skills.add(skillName)
+            } catch (IllegalArgumentException e) {
+                println("Habilidade Inválida. Por favor, escolha uma da lista!")
+            }
+        }
+
+        println("\n\u001B[36m${name}, seu cadastro foi efetuado!\u001B[0m")
+        def newCompany = new CompanyModel(name, email, cnpj, state, country, cep, description, skills)
     }
 
     List<ApplicantModel> listApplicants() {
